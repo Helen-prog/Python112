c10b6cacceb744c56ed73f565dc443e3b2e89ceb
Helen <elena.kozovyakina@gmail.com>
2022-03-30 21:24:54 +0300

    class

--- 112.py
+++ 112.py
@@ -4100,6 +4100,8 @@ import os.path
 # print(os.path.getctime("one.txt"))  # возваращается время создания файла
 
 import time
+
+
 #
 # path = r"D:\Python112\112.py"
 # size = os.path.getsize(path)
@@ -4130,5 +4132,202 @@ import time
 # else:
 #     print(f"File {str2} Not Found")
 
-print(os.path.isfile(r"D:\Python112\Work\F2\F21"))  # возвращает True, если путь является файлом
-print(os.path.isdir(r"D:\Python112\Work\F2\F21\f211.txt"))  # возвращает True, если путь является директорией
+# print(os.path.isfile(r"D:\Python112\Work\F2\F21"))  # возвращает True, если путь является файлом
+# print(os.path.isdir(r"D:\Python112\Work\F2\F21\f211.txt"))  # возвращает True, если путь является директорией
+
+# class Point3D:
+#     pass
+
+
+# class Point:
+#     """Класс для предоставления координат точек на плоскости"""
+#     x = 1
+#     y = 1
+#
+#     def set_coords(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#
+# p1 = Point()
+# p1.x = 100
+# p1.y = 300
+# p1.set_coords(5, 10)
+# print(p1.__dict__)
+# # Point.set_coords(p1, "Elena")
+#
+# p2 = Point()
+# p2.set_coords(3, 8)
+# print(p2.__dict__)
+# Point.set_coords(p2, "Igor")
+
+# print(type(p1))
+# print(isinstance(p1, Point3D))
+# print(p1.x, p1.y)
+# print(p1.__dict__)
+# print(getattr(p1, "x"))
+# print(getattr(p1, "z", "No"))
+#
+# print(hasattr(p1, "y"))
+# print(setattr(p1, "z", 7))
+# print(p1.__dict__)
+# print(hasattr(p1, "z"))
+# delattr(p1, "z")
+# print(p1.__dict__)
+# p2 = Point()
+# print(p2.x, p2.y)
+# print(p2.__dict__)
+# print(Point.__doc__)
+# print(Point.__name__)
+# print(dir(Point))
+
+# class Human:
+#     name = "name"
+#     birthday = "00.00.0000"
+#     phone = "00-00-00"
+#     country = "country"
+#     city = "city"
+#     address = "street, house"
+#
+#     def print_info(self):
+#         print(" Персональные данные ".center(40, "*"))
+#         print(f"Имя: {self.name}\nДата рождения: {self.birthday}\nНомер телефона: {self.phone}\nСтрана: "
+#               f"{self.country}\nГород: {self.city}\nДомашний адрес: {self.address}")
+#         print('=' * 40)
+#
+#     def input_info(self, first_name, birthday, phone, country, city, address):
+#         self.name = first_name
+#         self.birthday = birthday
+#         self.phone = phone
+#         self.country = country
+#         self.city = city
+#         self.address = address
+#
+#     def set_name(self, name):  # set - установить
+#         self.name = name
+#
+#     def get_name(self):  # get - получить
+#         return self.name
+#
+#     def set_phone(self, ph):
+#         self.phone = ph
+#
+#     def get_phone(self):
+#         return self.phone
+#
+#
+# h1 = Human()
+# h1.print_info()
+# h1.input_info("Юля", "23.05.1986", "45-46-98", "Россия", "Москва", "Чистопрудный бульвар, 1А")
+# h1.print_info()
+# h1.set_name("Егор")
+# print(h1.get_name())
+# h1.set_phone("58-99-00")
+# print(h1.get_phone())
+
+
+# class Person:
+#     skill = 10
+#
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#
+#     def print_info(self):
+#         print("Данные сотрудника: ", self.name, self.surname)
+#
+#     def add_skill(self, k):
+#         self.skill += k
+#         print(f"Квалификация сотрудника {self.name}: ", self.skill, "\n")
+#
+#
+# p1 = Person('Viktor', 'Reznik')
+# p1.print_info()
+# p1.add_skill(3)
+# p2 = Person('Anna', 'Dolgih')
+# p2.print_info()
+# p2.add_skill(2)
+
+# class Point:
+#     # def __new__(cls, *args, **kwargs):
+#     #     print("Consrtuctor")
+#     #     return super().__new__(cls)
+#
+#     def __init__(self, x=0, y=0):
+#         print("Initializator")
+#         self.x = x
+#         self.y = y
+#
+#     def __del__(self):
+#         print("Удаление экземпляра: " + self.__class__.__name__)
+#
+#     def set_coords(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#
+# p1 = Point(5, 10)
+# print(p1.__dict__)
+# # del p1
+# # print(p1.x)
+# p2 = Point()
+# print(p2.__dict__)
+
+
+# class Point:
+#     count = 0  # статическое свойство
+#
+#     def __init__(self, x=0, y=0):
+#         self.x = x
+#         self.y = y
+#         Point.count += 1
+#
+#
+# p1 = Point(5, 10)
+#
+# p2 = Point(10, 20)
+# p3 = Point(10, 20)
+#
+# print(Point.count)
+# print(p1.count)
+# print(p2.count)
+
+
+class Robot:
+    k = 0
+
+    def __init__(self, name):
+        self.name = name
+        print("Инициализация робота:", self.name)
+        print("Приветствую! Меня зовут:", self.name)
+        Robot.k += 1
+
+    def __del__(self):
+        print(self.name, "выключается!")
+        Robot.k -= 1
+
+        if Robot.k == 0:
+            print(self.name, "был последним")
+        else:
+            print("Работающих роботов осталось:", Robot.k)
+
+
+droid1 = Robot('R2-D2')
+print("Численность роботов:", Robot.k)
+
+droid2 = Robot('C-3PO')
+print("Численность роботов:", Robot.k)
+
+droid3 = Robot('QQ-D2')
+print("Численность роботов:", Robot.k)
+
+droid4 = Robot('WW-3PO')
+print("Численность роботов:", Robot.k)
+
+print("*" * 20)
+del droid1
+del droid2
+del droid3
+del droid4
+
+print("Численность роботов", Robot.k)
