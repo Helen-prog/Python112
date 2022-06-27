857f8bec3a932a798f1c8ac43fbfb326edab2c61
Helen <elena.kozovyakina@gmail.com>
2022-06-27 21:31:22 +0300

    flask-db

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -50,7 +50,7 @@ def close_db(error):
 def index():
     db = get_db()
     dbase = FDataBase(db)
-    return render_template("index.html", title="Главная", menu=dbase.get_menu())
+    return render_template("index.html", title="Главная", menu=dbase.get_menu(), posts=dbase.get_posts_anonce())
 
 
 @app.route("/about")
@@ -90,6 +90,35 @@ def profile(username):
     if 'userLogged' not in session or session['userLogged'] != username:
         abort(401)
     return f"Пользователь: {username}"
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def add_post():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash("Ошибка добавления статьи", category='error')
+            else:
+                flash("Статья добавлена успешно", category='success')
+        else:
+            flash("Ошибка добавления статьи", category='error')
+
+    return render_template("add_post.html", title="Добавление статьи", menu=dbase.get_menu())
+
+
+@app.route('/post/<alias>')
+def show_post(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.get_post(alias)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.get_menu(), title=title, post=post)
 
 
 if __name__ == '__main__':
