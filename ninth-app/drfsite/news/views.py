a935e76d321014a5b6d8537ad822d30716fdf18d
Helen <elena.kozovyakina@gmail.com>
2022-10-07 21:44:46 +0300

    test

--- ninth-app/drfsite/news/views.py
+++ ninth-app/drfsite/news/views.py
@@ -5,21 +5,49 @@ from .models import News
 from .serializers import NewsSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 
+
+class NewsAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+
+class NewsAPIList(generics.ListCreateAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer  # реализует GET и POST
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class NewsAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer  # реализует метод PUT
+    permission_classes = (IsOwnerOrReadOnly,)
+
+
+class NewsAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 # class NewsAPIView(generics.ListAPIView):
 #     queryset = News.objects.all()
 #     serializer_class = NewsSerializer
 
+
 class NewsAPIView(APIView):
     def get(self, request):
-        lst = News.objects.all().values()
-        return Response({'posts': list(lst)})
+        n = News.objects.all()
+        return Response({'posts': NewsSerializer(n, many=True).data})
 
     def post(self, request):
+        serializer = NewsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = News.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             category_id=request.data['category_id']
         )
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': NewsSerializer(post_new).data})
